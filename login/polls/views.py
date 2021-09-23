97eec7199d0b5ea9c391e45b4b6d1aa186c843c6
Luis <marqueseduardo188@gmail.com>
2021-09-23 11:51:33 -0300

    django

--- login/polls/views.py
+++ login/polls/views.py
@@ -1,6 +1,32 @@
+from django import template
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 def index(request):
     return HttpResponse("Olá amigos e amigas!")
+
+def detail(request, question_id):
+    return HttpResponse('Você está olhando para a pergunta %s.' % question_id)
+
+def results(request, question_id):
+    response = "Você está olhando os resultados da pergunta %s. " 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s." % question_id)
+
+'''def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))'''
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
